387fd12c161e39e5e2959c0c54edfcbd5c3ff1b7
Kuk Hoon Ryou <coocoon09@gmail.com>
2024-03-15 17:11:23 -0400

    pass but some issues

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Task from "./Task.js";
 
-function TaskList() {
+function TaskList({ tasks, deletedItem }) {
+      
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {tasks.map((task) => (
+      
+      <TaskList
+        tasks = {tasks}
+        key =  {task.text}
+        deletedItem = {deletedItem}
+        text = {task.text}
+        category = {task.category}
+      />
+      ))}
     </div>
   );
 }
